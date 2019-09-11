570f633f17129912e94c45edaf0400441a6ae80d
meishuai <1823852132@qq.com>
2019-09-11 17:10:24 +0800

    添加项目文件。

---
+++ QrCodeText.Test/UnitTest1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ZXing;
+using ZXing.Common;
+using ZXing.QrCode;
+
+namespace QrCodeText.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void ReadQrCode()
+        {
+            var bar = new BarcodeReader();
+            bar.Options = new DecodingOptions {
+                PossibleFormats = new List<BarcodeFormat> { BarcodeFormat.QR_CODE }
+            };
+            var bitmap = (Bitmap)Bitmap.FromFile("D:\\a.png");
+            var result = bar.Decode(bitmap);
+            Console.WriteLine(result.Text);
+        }
+    }
+}
---
+++ QrCodeText/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ZXing;
+using ZXing.Common;
+
+namespace QrCodeText
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new OpenFileDialog();
+            if (dialog.ShowDialog(App.Current.MainWindow) == true)
+            {
+                string path = dialog.FileName;
+                this.Image1.Source = new BitmapImage(new Uri(path));
+
+                var bar = new BarcodeReader();
+                bar.Options = new DecodingOptions
+                {
+                    PossibleFormats = new List<BarcodeFormat> { BarcodeFormat.QR_CODE }
+                };
+                var bitmap = (Bitmap)Bitmap.FromFile(path);
+                var result = bar.Decode(bitmap);
+                this.Text1.Text = result.Text;
+            }
+
+        }
+    }
+}
